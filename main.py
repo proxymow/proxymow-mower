8bb910b2403ee3766113c55cec98fd84e7aecccb
proxymow <dev@proxymow.co.uk>
2026-05-06 08:51:17 +0100

    added exception logging to file

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import schematic
+import sys
+import machine as m
 import micropython
+import schematic
 
 # enable emergency exception buffer for exceptions in isrs
 micropython.alloc_emergency_exception_buf(100)
 
-# then finally
-import mower
+try:
+    # load mower
+    print('launching mower')
+    import mower
+except Exception as err:
+    msg = 'error launching mower: ' + str(err)
+    print(msg)
+    # Open the file in 'append' mode to keep previous error logs
+    with open("error_log.txt", "a") as f:
+        f.write("\n--- main Error ---\n")
+        sys.print_exception(err, f)
+    m.reset()
