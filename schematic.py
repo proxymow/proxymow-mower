883f77b3673006c7299b035e2b76c28fc21473cf
proxymow <dev@proxymow.co.uk>
2026-05-06 08:53:19 +0100

    support for ESP8266 discontinued

--- schematic.py
+++ schematic.py
@@ -1,9 +1,11 @@
-from machine import Pin, I2C, Timer, RTC, PWM, ADC, unique_id
+from machine import Pin, I2C, Timer, RTC, PWM, ADC, unique_id, reset_cause
 import ubinascii
 import network
-from utils import log, led
+from utils import log
+from umotion_lib import led
 from time import sleep
-dev_type = 'esp8266' # default
+from sys import platform # esp32 | rp2
+mower_name = 'Unnamed'
 # name                      board label
 i2c_scl = 5     #           GPIO5    D1
 i2c_sda = 4     #           GPIO4    D2             
@@ -18,7 +20,6 @@ MOTOR_PWM_DUTY_16 = 65535
 MOTOR_PWM_INVERT = False
 RAMP_UP_TIME_MS = 300
 RAMP_DOWN_TIME_MS = 0
-REPL_SPACE = 0.2   # 0.2 secs for other tasks
 RELAY_STATE_REG = 10
 i2c_reg = 0x52 # mLink
 axle_track_m = 0 # m
@@ -34,8 +35,7 @@ adc_enabled = True
 adcs = []
 i2c_enabled = True
 pwms = []
-# create Real Time Clock
-rtc = RTC()
+
 # customise h/w interfaces according to unique identifier
 uid = ubinascii.hexlify(unique_id()).decode().upper()
 if uid == '6789ABCD':
@@ -46,11 +46,9 @@ if uid == '6789ABCD':
 elif uid == 'E661380123456789':
     # pico RP2
     mower_name = 'Pico1'
-    dev_type = 'rp2'
 elif uid == '6789ABCDEFGH':
     # esp32
-    mower_name = 'ESP32-A'
-    dev_type = 'esp32'
+    mower_name = 'ESP32A'
     left_in1 = 16 # esp32 gpio0 => gpio16
     left_in2 = 17 # esp32 gpio14 => gpio17
     right_in1 = 18 # esp32 gpio12 => gpio18
@@ -59,7 +57,6 @@ elif uid == '6789ABCDEFGH':
 else:
     # unidentified id - use defaults
     log('Unidentified Identifier uid: ' + uid)
-    mower_name = 'Unknown'
 out_pin_init_state = [MOTOR_PWM_INVERT] * 4 + [False]
 out_pin_numbers = [left_in1, left_in2, right_in1, right_in2, act_led]
 # make all out pins outputs
@@ -81,54 +78,44 @@ for i in range(pin_count):
     in_pins[name] = pin
 log(str(pin_count) + ' in pins initialised')
 # create timers
-if dev_type == 'esp32':
-    act_timer = Timer(0) # create mSec timer for activate delay
-    tt_dur_timer = Timer(1) # create mSec timer for led duration
+if platform == 'esp32':
+    act_timer = Timer(1) # create mSec timer for activate delay
+    led_on_timer = Timer(2) # create mSec timer for led on
+    led_off_timer = Timer(3) # create mSec timer for led off
 else:
     act_timer = Timer(-1)
-    tt_dur_timer = Timer(-1)
-if dev_type == 'esp8266':
-    pwms = [
-        PWM(out_pins["left_in2"], MOTOR_PWM_FREQ, MOTOR_PWM_DUTY_10 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["left_in1"], MOTOR_PWM_FREQ, MOTOR_PWM_DUTY_10 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["right_in2"], MOTOR_PWM_FREQ, MOTOR_PWM_DUTY_10 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["right_in1"], MOTOR_PWM_FREQ, MOTOR_PWM_DUTY_10 if MOTOR_PWM_INVERT else 0)
-    ]
-else:
-    pwms = [
-        PWM(out_pins["left_in2"], freq=MOTOR_PWM_FREQ, duty_u16=MOTOR_PWM_DUTY_16 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["left_in1"], freq=MOTOR_PWM_FREQ, duty_u16=MOTOR_PWM_DUTY_16 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["right_in2"], freq=MOTOR_PWM_FREQ, duty_u16=MOTOR_PWM_DUTY_16 if MOTOR_PWM_INVERT else 0),
-        PWM(out_pins["right_in1"], freq=MOTOR_PWM_FREQ, duty_u16=MOTOR_PWM_DUTY_16 if MOTOR_PWM_INVERT else 0)
-    ]
-    for pwm in pwms: # ensure stopped
-        if pwm.duty_u16() != 0:
-            log('motor pwm {} failed to initialise - resetting duty'.format(pwm))
-            pwm.duty_u16(0)
+    led_on_timer = Timer(-1)
+    led_off_timer = Timer(-1)
+stop_duty = MOTOR_PWM_DUTY_16 if MOTOR_PWM_INVERT else 0
+pwms = [
+    PWM(out_pins["left_in2"], freq=MOTOR_PWM_FREQ, duty_u16=stop_duty),
+    PWM(out_pins["left_in1"], freq=MOTOR_PWM_FREQ, duty_u16=stop_duty),
+    PWM(out_pins["right_in2"], freq=MOTOR_PWM_FREQ, duty_u16=stop_duty),
+    PWM(out_pins["right_in1"], freq=MOTOR_PWM_FREQ, duty_u16=stop_duty)
+]
+for pwm in pwms: # ensure stopped
+    if abs(pwm.duty_u16() - stop_duty) > 10:
+        log('motor pwm {} failed to initialise 16bit duty to {} - resetting duty'.format(pwm, stop_duty))
+        pwm.duty_u16(stop_duty)
 log('motor pwms initialised uid:{} Name:{} pwms:{}'.format(uid, mower_name, pwms))
 # construct an I2C bus
 if i2c_enabled:
-    if dev_type == 'esp8266':
-        i2c = I2C(scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=500000)
-    else:
-        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=500000)
+    i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=500000)
     log('i2c enabled: {}'.format(i2c.scan()))
 # adc
 if adc_enabled:
-    if dev_type == 'esp8266':
-        chans = [0]
-    elif dev_type == 'rp2':
+    if platform == 'rp2':
         # channel number in the range 0 - 3 or ADC.CORE_TEMP
         # pin GP26..GP29 on pico
         chans = [0, 1, 2, 3, ADC.CORE_TEMP]
-    elif dev_type == 'esp32':
+    elif platform == 'esp32':
         # pin number in the range 32 - 39
         chans = [36, 39, 34, 35, 32, 33]
     for chan in chans:
         adc = ADC(chan)
-        if dev_type == 'esp32': adc.atten(ADC.ATTN_11DB) # 3.3V range
+        if platform == 'esp32': adc.atten(ADC.ATTN_11DB) # 3.3V range
         adcs.append(adc)
     log('{} adc(s) initialised'.format(len(chans)))
 for _ in range(4):
     led(400);sleep(1);led(100);sleep(1)
-log('Schematic for {} {} Initialised'.format(mower_name, dev_type))
+log('Schematic for {} {} Initialised'.format(mower_name, platform))
