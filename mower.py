c706cab8a19bb8f751c0a03a5f4b45cdb197b575
proxymow <dev@proxymow.co.uk>
2026-05-06 08:52:28 +0100

    dual udp and bluetooth listener

--- mower.py
+++ mower.py
@@ -1,12 +1,15 @@
+from sys import print_exception, platform # esp32 | rp2
 import os
 import socket
+import select
 import re
+import utils as ut
+from time import sleep
+from utime import ticks_ms, ticks_diff
+import machine as m
+
 import umotion_lib # needed to allow process to call functions
 from umotion_lib import led, cutter
-from utils import log, checks
-from time import sleep
-from utime import ticks_ms, ticks_diff, time
-import machine as m
 import schematic as scm
 
 def process_cmd(multi_cmd, trace=False):
@@ -26,10 +29,10 @@ def process(cmd, trace):
     try:
         msg = 'Processing: ' + cmd
         if trace:
-            log(msg)
+            ut.log(msg)
         cmd_parts = cmd.split('(')
         if (len(cmd_parts) == 1):
-            log('Error in cmd: ' + cmd)
+            ut.log('Error in cmd: ' + cmd)
         else:
             instr = cmd_parts[0]
             param_str = cmd_parts[1][:-1]
@@ -37,82 +40,128 @@ def process(cmd, trace):
             if param_str != '':
                 if ',' not in param_str:
                     if trace:
-                        log('Single parameter: ' + param_str)
+                        ut.log('Single parameter: ' + param_str)
                     params = [param_str]
                 else:
                     if trace:
-                        log('Splitting Multiple parameters...')
+                        ut.log('Splitting Multiple parameters...')
                     params = [float(x) for x in param_str.split(',')]
             result = getattr(umotion_lib, instr)(*params)
             if trace:
-                log('Result: ' + str(result))
+                ut.log('Result: ' + str(result))
     except Exception as e:
         result = str(e)
     return result
-    
-log('Initialising socket...')
-# initialise socket
-HOST = '0.0.0.0'        # Standard loopback interface address (localhost)
+
+try:
+    # Create and start BLE UART service
+    from blue_uart import BLE
+    ut.log('Starting BLE UART...')
+    ble = BLE(
+        scm.mower_name,
+        scm.out_pins['act_led'],
+        scm.led_on_timer,
+        scm.led_off_timer
+    )
+except Exception as e:
+    ut.log('Problem launching blue uart')
+HOST = '0.0.0.0'        # All addresses interface address
 PORT = 5005             # UDP Port to listen on (non-privileged ports are > 1023)
 ACK = 'ACK'
-TIMEOUT = 30            # 30 seconds between checks if offline
-NUM_TIMEOUTS_RST = 3	# Number of timeouts before rebooting
-last_num_re = re.compile('[,)\s]+')
+MAX_POLL_TIMEOUT = 1000 # 1 second
+MIN_POLL_TIMEOUT = 100 # 100 msecs
+CHECK_MS = 60000 # ms between checks if offline
+NUM_CHECKS_RST = 3 # Number of checks before rebooting
+last_num_re = re.compile('[,()\s]+')
 
+ut.log('Initialising udp socket...')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST, PORT))
-s.settimeout(TIMEOUT)
-log('listening on UDP Port ' + str(PORT))
+pollerObject = select.poll()
+pollerObject.register(s, select.POLLIN)
+ut.log('listening on UDP Port: ({})'.format(PORT))
 keep_going = True
-
-timeouts = 0
-checks() # boot checks to get online
-
+timeout = MAX_POLL_TIMEOUT
+num_checks = 0
+last_req = ticks_ms()
+ut.checks() # boot checks to get online
+bt = None
 while keep_going:
     try:
-        line, addr = s.recvfrom(1024)
-        timeouts = 0
+        events = pollerObject.poll(timeout)
+        line = None
+        if len(events) > 0:
+            bt = False
+            timeout = MAX_POLL_TIMEOUT
+            event = events[0]
+            line, addr = s.recvfrom(64)
+        elif ble.flag:
+            line = ble.request
+            ble.flag = False
+            bt = True
+            timeout = MIN_POLL_TIMEOUT
+        if bt is True:
+            ut.telem['essid'] = 'bluetooth'
+            ut.telem['rssi'] = -50
+        elif bt is False:
+            rssi = ut.get_rssi()
+            ut.telem['rssi'] = rssi
+            essid = ut.get_essid()
+            ut.telem['essid'] = essid
         if line:
+            last_req = ticks_ms()
+            num_checks = 0
+            led(100)
             cmd = line.decode("utf-8").strip()
             if cmd[0] == '>':
                 # Synchronous Request - process before replying
                 cmd = cmd[1:]
                 is_pose_cmd = 'pose' in cmd
                 if not is_pose_cmd:
-                    log('Incoming synchronous request: ' + cmd + ' from: ' + str(addr))
-                sleep(scm.REPL_SPACE)
+                    ut.log('Synchronous {} request: {}'.format(
+                        'bluetooth' if bt else 'network', cmd))
                 result = process_cmd(cmd, False)
                 if not is_pose_cmd:
-                    log('Processed result: ' + str(result))
+                    ut.log('Processed result: ' + str(result))
                 response = str(result).encode()
-                s.sendto(response, addr)
+                if bt:
+                    try:
+                        ble.send(response)
+                    except:
+                        ut.log('ble response failed')    
+                else:
+                    s.sendto(response, addr)
                 if not is_pose_cmd:
-                    log('Sent response')
+                    ut.log('Sent response')
             else:
                 # Asynchronous Request - process after replying ack
-                log('Incoming asynchronous request: ' + cmd + ' from: ' + str(addr))
+                ut.log('Asynchronous {} request: {}'.format(
+                    'bluetooth' if bt else 'network', cmd))
                 resp = last_num_re.split(cmd)
-                s.sendto(ACK + '#' + resp[-2], addr)
-                log('Sent acknowledgement')            
+                response = ACK + '#' + resp[-2:][0]
+                if bt:
+                    ble.send(response)
+                else:
+                    s.sendto(response, addr)
+                ut.log('Sent acknowledgement')            
                 result = process_cmd(cmd, True)
-                log('Processed result: ' + str(result))
+                ut.log('Processed result: ' + str(result))
         else:
-            log('Incoming request breaking!')
-        led(100)
-    except OSError as err:
-        timeouts += 1
-        if timeouts > NUM_TIMEOUTS_RST:
-            log('timeout count exceeded - performing reset...')
-            # disable cutters to reduce emi
-            cutter(0, -1)
+            # no incoming request
+            timeout = int((timeout + MAX_POLL_TIMEOUT) / 2)
+        if ticks_diff(ticks_ms(), last_req) > CHECK_MS:
+            num_checks += 1
+            if num_checks >= NUM_CHECKS_RST:
+                ut.log('no contact - performing reset...')
+                # disable cutters to reduce emi
+                cutter(0, -1)
+                sleep(5)
+                m.reset()
+            ut.log('no contact - performing checks {}'.format(num_checks))
             sleep(5)
-            m.reset()
-        else:
-            led(500)
-            sleep(5)
-            checks()
+            ut.checks()
+            last_req = ticks_ms()
     except Exception as err:
         msg = 'mower error: ' + str(err)
-        log(msg)
-        sleep(30)
-        m.reset()
+        ut.log(msg)
+        raise(err)
